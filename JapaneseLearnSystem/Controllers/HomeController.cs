3d963217fe2a28564b82432f33622f7194157bec
kaze <yoyo2050123@gmail.com>
2025-09-10 00:25:47 +0800

    9/10 HomeController筆記

--- JapaneseLearnSystem/Controllers/HomeController.cs
+++ JapaneseLearnSystem/Controllers/HomeController.cs
@@ -35,3 +35,7 @@ namespace JapaneseLearnSystem.Controllers
 //從github抓下來的程式碼 記得修改dbJapaneseLearnSystemContext.cs跟Program.cs的連線字串
 //就是"Data Source=KAZE;Database=dbJapaneseLearnSystem;TrustServerCertificate=True;User ID=abc;Password=123"這串
 //記得以後要修改Program.cs的話 也要同步修改這邊的連線字串，就是"dbJapaneseLearnSystemConnection"，這個要跟appsettings.json的連線字串名稱一樣
+
+//_ViewStart.cshtml要放在Views資料夾裡面那層，不要放進去Shared資料夾裡面
+//資料中介表格的命名規則是兩個資料表名稱中間用底線連接起來，然後兩個資料表名稱都要用複數形式
+//資料中介表要Scaffold的話要注意，因為他沒有辦法手動去修改，所以要多建立一個空白的欄位來讓它變成實體，這樣Scaffold才會能夠手動操作這個表格
